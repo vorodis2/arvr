4616c9cdb978bf28c6ca418175109617575c0390
Stein256 <izaya256@gmail.com>
2020-03-18 15:50:20 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const bodyParser = require('body-parser');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const root = path.resolve(__dirname, '.');
-const dest = path.resolve(__dirname, 'build');
+const dest = path.resolve(__dirname, 'docs');
 
 let conf = {
 	context: root,
