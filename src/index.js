1f4351dc9033f300cd3600e7b4e8b67c9d685506
Stein256 <izaya256@gmail.com>
2020-03-18 14:02:38 +0200

    aframe first steps

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three'
+
+const scene = document.querySelector('#portal-contents').object3D
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
